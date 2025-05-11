5e9f691313e1e2c97be794aed08837a68bb799fe
mjdutra <130234028+mjdutra@users.noreply.github.com>
2025-05-11 16:54:14 +0100

    Mega Maximus Maria Again w/ js
    - Desistimos de fazer no python e avançamos com js, css e html em força!

---
+++ script.js
@@ -0,0 +1,14 @@
+// function enviarDados() {
+//   const nome = document.getElementById('nome').value;
+//   const descricao = document.getElementById('descricao').value;
+
+//   fetch('/api/enviar', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ nome, descricao })
+//   })
+
+//   .then(response => response.json())
+//   .then(data => alert(data.mensagem))
+//   .catch(error => console.error('Erro:', error));
+// }
